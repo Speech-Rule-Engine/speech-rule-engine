88468d48a9bc65fbb0424c06cfeebfe6bcb394d9
zorkow <volker.sorge@gmail.com>
2014-08-01 05:01:50 +0100

    Added Api.

---
+++ src/common/api.js
@@ -0,0 +1,50 @@
+// Copyright 2014 Volker Sorge
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+/**
+ * @fileoverview API of the Speech Rule Engine.
+ *
+ * @author volker.sorge@gmail.com (Volker Sorge)
+ */
+goog.provide('sre.Api');
+
+goog.require('sre.System');
+
+
+sre.Api = { };
+
+
+sre.Api.processExpression = function(expr) {
+  return sre.System.getInstance().processExpression(expr);
+};
+
+
+
+/**
+ * Exporting method to process an expression.
+ */
+module.exports.processExpression = sre.Api.processExpression;
+
+
+/**
+ * Exporting method to process expression in files.
+ */
+module.exports.processFile = sre.System.getInstance().processFile;
+
+
+/**
+ * Exporting method to set up and parameterise the Engine.
+ */
+module.exports.setupEngine = sre.System.getInstance().setupEngine;
