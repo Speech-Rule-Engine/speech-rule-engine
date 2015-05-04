93980faf009c5904d5a98e7809b123825f7edfcb
zorkow <volker.sorge@gmail.com>
2015-05-04 02:08:10 +0100

    Fixes order of tests.

--- tests/tests.js
+++ tests/tests.js
@@ -56,11 +56,11 @@ sre.Tests.prototype.run = function() {
  * @type {Array}
  */
 sre.Tests.testList = [
-  sre.SemanticTreeTest,
   sre.MathmlCloudTest,
   sre.MathmlStoreTest,
   sre.MathspeakRuleTest,
   sre.SemanticRuleTest,
+  sre.SemanticTreeTest,
   sre.SpeechRuleTest
 ];
 
