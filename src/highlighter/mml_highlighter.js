452bbbd0d681638dd25219ef89d22ce18c5d989c
zorkow <volker.sorge@gmail.com>
2016-09-03 22:18:26 +0100

    Solves issue #90.

--- src/highlighter/mml_highlighter.js
+++ src/highlighter/mml_highlighter.js
@@ -77,3 +77,11 @@ sre.MmlHighlighter.prototype.colorString = function() {
 sre.MmlHighlighter.prototype.getMactionNodes = function(node) {
   return node.getElementsByTagName(this.mactionName);
 };
+
+
+/**
+ * @override
+ */
+sre.MmlHighlighter.prototype.isMactionNode = function(node) {
+  return node.tagName === this.mactionName;
+};
