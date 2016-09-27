d14b12689c8c05aecfba5b93b514c462b9191e59
zorkow <volker.sorge@gmail.com>
2016-09-27 10:47:13 +0100

    Exports functionality necessary for semantic tree generation via the semantic-node API.

--- src/semantic_tree/semantic_api.js
+++ src/semantic_tree/semantic_api.js
@@ -21,6 +21,8 @@
 goog.provide('sre.SemanticApi');
 
 goog.require('sre.Semantic');
+goog.require('sre.SemanticNodeFactory');
+goog.require('sre.SemanticProcessor');
 goog.require('sre.SemanticTree');
 
 
@@ -42,4 +44,62 @@ module.exports.Role = sre.Semantic.Role;
 module.exports.Type = sre.Semantic.Type;
 
 
-// module.exports.branchNode = sre.SemanticTree.makeBranchNode_;
+// TODO: This API should eventually be slimmed down.
+/**
+ * Exports node factory.
+ */
+module.exports.NodeFactory = sre.SemanticNodeFactory;
+
+
+/**
+ * Exports semantic tree.
+ */
+module.exports.SemanticTree = sre.SemanticTree;
+
+
+/**
+ * Exports semantic processing.
+ */
+module.exports.SemanticProcessor = {};
+
+
+/**
+ * Setting of node factory for processing.
+ */
+module.exports.SemanticProcessor.setFactory =
+  sre.SemanticProcessor.getInstance().setNodeFactory;
+
+
+module.exports.SemanticProcessor.row =
+  sre.SemanticProcessor.getInstance().row;
+
+module.exports.SemanticProcessor.identifierNode =
+  sre.SemanticProcessor.getInstance().identifierNode;
+
+module.exports.SemanticProcessor.fractionLikeNode =
+  sre.SemanticProcessor.getInstance().fractionLikeNode;
+
+module.exports.SemanticProcessor.limitNode =
+  sre.SemanticProcessor.getInstance().limitNode;
+
+module.exports.SemanticProcessor.text =
+  sre.SemanticProcessor.getInstance().text;
+
+module.exports.SemanticProcessor.number =
+  sre.SemanticProcessor.getInstance().number;
+
+module.exports.SemanticProcessor.mfenced =
+  sre.SemanticProcessor.getInstance().mfenced;
+
+module.exports.SemanticProcessor.tablesInRow =
+  sre.SemanticProcessor.getInstance().tablesInRow;
+
+module.exports.SemanticProcessor.tableToMultiline =
+  sre.SemanticProcessor.tableToMultiline;
+
+module.exports.SemanticProcessor.pseudoTensor =
+  sre.SemanticProcessor.getInstance().pseudoTensor;
+
+module.exports.SemanticProcessor.tensor =
+  sre.SemanticProcessor.getInstance().tensor;
+
