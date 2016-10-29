fd790c1b2fc9c324c4371e4c5030a942c10e0896
zorkow <volker.sorge@gmail.com>
2016-10-28 23:35:26 -0700

    Correct shallow speech generation.

--- src/common/system.js
+++ src/common/system.js
@@ -246,13 +246,8 @@ sre.System.prototype.toEnriched = function(expr) {
   var root = sre.WalkerUtil.getSemanticRoot(enr);
   switch (sre.Engine.getInstance().speech) {
     case sre.Engine.Speech.SHALLOW:
-      var speech = sre.System.getInstance().toSpeech(expr);
-      root.setAttribute(sre.EnrichMathml.Attribute.SPEECH, speech);
-      // The following is how it should be done. But there are still some
-      // problems with tables and embellished elements that make rebuilt
-      // difficult.
-      //
-      // (new sre.AdhocSpeechGenerator()).getSpeech(root, enr);
+      var generator = sre.SpeechGeneratorFactory.generator('Adhoc');
+      generator.getSpeech(root, enr);
       break;
     case sre.Engine.Speech.DEEP:
       var generator = sre.SpeechGeneratorFactory.generator('Tree');
