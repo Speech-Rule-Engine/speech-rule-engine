743c7bbb2305a496fbb9a2573d8addc2ab05709e
zorkow <volker.sorge@gmail.com>
2017-04-11 15:43:09 +0100

    Ensures CLI input for markup is passed on.

--- src/common/cli.js
+++ src/common/cli.js
@@ -87,7 +87,7 @@ sre.Cli.prototype.commandLine = function() {
       option('-j, --json', 'Generate JSON of semantic tree.').
       option('-m, --mathml', 'Generate enriched MathML.').
       option('-p, --speech', 'Generate speech output (default).').
-      option('-k, --markup', 'Generate speech output with markup tags.').
+      option('-k, --markup [name]', 'Generate speech output with markup tags.').
       option('-x, --xml', 'Generate XML of semantic tree.').
       option('').
       option('-v, --verbose', 'Verbose mode.').
