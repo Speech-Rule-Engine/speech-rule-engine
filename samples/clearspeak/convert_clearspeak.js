6d73410890ca9c0a4f7fbad0dcdd06ee632109be
zorkow <volker.sorge@gmail.com>
2017-04-21 17:33:18 +0100

    Removes placeholder test files.

--- samples/clearspeak/convert_clearspeak.js
+++ samples/clearspeak/convert_clearspeak.js
@@ -1,47 +1,66 @@
 var xmldom = require('xmldom');
 
 var files = [
-  "ClearSpeakExamples_AbsoluteValue",
-  "ClearSpeakExamples_CapitalLetters",
-  "ClearSpeakExamples_Exponents",
-  "ClearSpeakExamples_Fractions",
-  "ClearSpeakExamples_Functions",
-  "ClearSpeakExamples_ImpliedTimes",
-  "ClearSpeakExamples_Logarithms",
-  "ClearSpeakExamples_Matrices_Vectors_and_Combinatorics",
-  "ClearSpeakExamples_Multi-Line_Entries",
-  "ClearSpeakExamples_NamedSets",
-  "ClearSpeakExamples_Parentheses",
-  "ClearSpeakExamples_Part2-Symbols",
-  "ClearSpeakExamples_Part_3_Adornments",
-  "ClearSpeakExamples_Roots",
-  "ClearSpeakExamples_SetsEnclosedInSetBrackets",
-  "ClearSpeakExamples_Trigometry"
+  'ClearSpeakExamples_AbsoluteValue',
+  'ClearSpeakExamples_CapitalLetters',
+  'ClearSpeakExamples_Exponents',
+  'ClearSpeakExamples_Fractions',
+  'ClearSpeakExamples_Functions',
+  'ClearSpeakExamples_ImpliedTimes',
+  'ClearSpeakExamples_Logarithms',
+  'ClearSpeakExamples_Matrices_Vectors_and_Combinatorics',
+  'ClearSpeakExamples_Multi-Line_Entries',
+  'ClearSpeakExamples_NamedSets',
+  'ClearSpeakExamples_Parentheses',
+  'ClearSpeakExamples_Part2-Symbols',
+  'ClearSpeakExamples_Part_3_Adornments',
+  'ClearSpeakExamples_Roots',
+  'ClearSpeakExamples_SetsEnclosedInSetBrackets',
+  'ClearSpeakExamples_Trigometry'
 ];
 
 
 const dp = new xmldom.DOMParser();
 const path = '/home/sorge/git/speech-rule-engine/samples/clearspeak/html/';
+const dest = '/home/sorge/git/speech-rule-engine/tests/clearspeak_tests/';
+
+let allFiles = {};
 
 loadFiles = function() {
   for (var i = 0, file; file = files[i]; i++) {
-    var input = fs.readFileSync(path + file + '.js', {encoding: 'utf8'});
+    var input = fs.readFileSync(path + file + '.html', {encoding: 'utf8'});
     var xml = dp.parseFromString(input);
     var struct = new theFile(file);
     processFile(struct, xml);
-    console.log(struct.toString());
+    allFiles[struct.name] = struct;
+  }
+};
+
+
+saveFiles = function() {
+  for (var key in allFiles) {
+    var file = allFiles[key];
+    console.log('Writing ' + file.fileName);
+    fs.writeFileSync(dest + file.fileName + '.js', file.toString());
   }
 };
 
 
 theFile = function(name) {
   this.name = name;
-  this.sre = 'sre.' + name.replace(/Examples_/, '');
+  this.shortName = name.replace(/ClearSpeakExamples_/, 'Clearspeak')
+    .replace(/_and/, 'And')
+    .replace(/_/g, '')
+    .replace(/-/g, '');
+  this.fileName = this.shortName
+    .replace('Clearspeak', 'clearspeak')
+    .replace(/([A-Z]{1})/, '_$1');
+  this.sre = 'sre.' + this.shortName;
   this.elements = [];
 };
 
 theFile.prototype.toString = function() {
-  var str = this.name + ':\n';
+  var str = this.fileStart();
   for (var i = 0, element; element = this.elements[i]; i++) {
     str += '\n' + this.elements[i].toString();
   }
@@ -50,26 +69,39 @@ theFile.prototype.toString = function() {
 
 theHeader = function(content) {
   this.content = content;
+  this.preference = '';
 };
 
 theHeader.prototype.toString = function() {
-  return '//\n// ' + this.content.replace(/\n/, '') + '\n//';
+  return '\n\n//\n// ' + this.content.replace(/\n/, '') + '\n//';
 };
 
 
-theElement = function(preference, identifier, example, speech) {
+theElement = function(preference, identifier, example, speech, file) {
   this.preference = preference;
   this.identifier = identifier;
   this.example = example;
   this.speech = speech;
+  this.file = file;
 };
 
 
 theElement.prototype.toString = function() {
-  var str = '' + this.identifier + ' = {';
-  str += '\n  var preference = \"' + this.preference + '\";';
-  str += '\n  var mathml = ' + this.example.toString();
-  str += '\n  var speech = ' + this.speech;
+  var str = '\n\n';
+  str += '/**\n * Testing ' + this.file.shortName.replace('/ClearSpeak/', '') +
+    ' Example ' + this.identifier + '\n */\n';
+  str += '' + this.file.sre + '.prototype.untest' + this.identifier + ' = function() {';
+  str += '\n  var preference = \'' + this.preference + '\';';
+  str += '\n  var mathml = \'' + this.example.childNodes[0].toString()
+    .replace(/<\/?semantics>/g, '')
+    .replace(' style="background-color:#"', '')
+    .replace(/>\s*</g, '><') +
+    '\';';
+  str += '\n  var speech = \'' + this.speech.toString()
+    .replace(/^<td>/, '')
+    .replace(/<\/td>$/, '') +
+    '\';';
+  str += '\n  this.executeRuleTest(mathml, speech, preference);';
   str += '\n};';
   return str;
 };
@@ -77,9 +109,26 @@ theElement.prototype.toString = function() {
 
 processFile = function(file, xml) {
   var nodes = childArray(xml);
+  var currentHeader = null;
+  var currentPreference = '';
   for (var i = 0, node; node = nodes[i]; i++) {
     if (node.tagName === 'h1' && node.textContent) {
-      file.elements.push(new theHeader(node.textContent));
+      currentHeader = new theHeader(node.textContent);
+      currentHeader.preference = currentPreference;
+      file.elements.push(currentHeader);
+    }
+    if (node.tagName === 'p' && node.textContent &&
+        node.textContent.match(/^Preference Name:/)) {
+      if (!currentHeader) {
+        throw file.name + ': Header missing!';
+      }
+      if (currentHeader.preference) {
+        console.log('WARNING: Preference for header ' + currentHeader.content +
+                    ' already set to: ' + currentHeader.preference +
+                    '. Resetting!');
+      }
+      currentPreference = node.textContent.replace(/^Preference Name:[ ]*/, '');
+      currentHeader.preference = currentPreference;
     }
     if (node.tagName === 'table') {
       // var nnnn = node.childNodes;
@@ -98,11 +147,12 @@ processFile = function(file, xml) {
         var cells = childArray(row);
         if (pref) {
           var preference = cells[0].textContent;
-          var element = new theElement(preference,
-                                       file.sre + '.prototype.' + cells[1].textContent, cells[2], cells[3]);
+          var element = new theElement(
+            currentHeader.preference + '_' + preference,
+            cells[1].textContent, cells[2], cells[3], file);
         } else {
-          var element = new theElement('Auto',
-                                       file.sre + '.prototype.' + cells[0].textContent, cells[1], cells[2]);
+          element = new theElement(
+            'default', cells[0].textContent, cells[1], cells[2], file);
         }
         file.elements.push(element);
       }
@@ -125,4 +175,58 @@ toArray = function(nodeList) {
     }
   }
   return nodeArray;
+};
+
+theFile.prototype.fileStart = function() {
+  var str = '// Copyright 2017 Volker Sorge';
+  str += '\n//';
+  str += '\n// Licensed under the Apache License, Version 2.0 (the "License");';
+  str += '\n// you may not use this file except in compliance with the License.';
+  str += '\n// You may obtain a copy of the License at';
+  str += '\n//';
+  str += '\n//      http://www.apache.org/licenses/LICENSE-2.0';
+  str += '\n//';
+  str += '\n// Unless required by applicable law or agreed to in writing, software';
+  str += '\n// distributed under the License is distributed on an "AS IS" BASIS,';
+  str += '\n// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.';
+  str += '\n// See the License for the specific language governing permissions and';
+  str += '\n// limitations under the License.';
+  str += '\n';
+  str += '\n//';
+  str += '\n// With support from the Mozilla Foundation under a MOSS grant.';
+  str += '\n//';
+  str += '\n';
+  str += '\n/**';
+  str += '\n* @fileoverview Testcases for clearspeak speech rules.';
+  str += '\n* @author Volker.Sorge@gmail.com (Volker Sorge)';
+  str += '\n*/';
+  str += '\n';
+  str += '\ngoog.provide(\'' + this.sre + '\');';
+  str += '\n';
+  str += '\ngoog.require(\'sre.ClearspeakRuleTest\');';
+  str += '\n';
+  str += '\n';
+  str += '\n';
+  str += '\n/**';
+  str += '\n* @constructor';
+  str += '\n* @extends {sre.ClearspeakRuleTest}';
+  str += '\n*/';
+  str += '\n' + this.sre + ' = function() {';
+  str += '\n' + this.sre + '.base(this, \'constructor\');';
+  str += '\n';
+  str += '\n/**';
+  str += '\n* @override';
+  str += '\n*/';
+  str += '\nthis.information = \'' + this.shortName + ' rule tests.\';';
+  str += '\n';
+  str += '\n};';
+  str += '\ngoog.inherits(' + this.sre + ', sre.ClearspeakRuleTest);';
+  str += '\n';
+  return str;
+};
+
+
+convertFiles = function() {
+  loadFiles();
+  saveFiles();
 };
