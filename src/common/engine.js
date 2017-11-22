3609a5a9c4749e0f1c105623633b47c4908d9f12
zorkow <volker.sorge@gmail.com>
2017-11-22 01:01:37 -0800

    Removes duplicate definition of locale in engine.

--- src/common/engine.js
+++ src/common/engine.js
@@ -84,14 +84,6 @@ sre.Engine = function() {
    */
   this.style = 'short';
 
-
-  /**
-   * Current locale.
-   * @type {string}
-   */
-  this.locale = sre.DynamicCstr.DEFAULT_VALUES[sre.DynamicCstr.Axis.LOCALE];
-
-
   /**
    * Current locale.
    * @type {string}
