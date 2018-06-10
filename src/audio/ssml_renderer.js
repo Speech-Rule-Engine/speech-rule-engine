18324a5f1b7dd8aca00c1d15b3332eb5ea0b7dd0
zorkow <volker.sorge@gmail.com>
2018-06-10 15:57:51 -0700

    Rounding for SSML attribute values.

--- src/audio/ssml_renderer.js
+++ src/audio/ssml_renderer.js
@@ -58,7 +58,7 @@ sre.SsmlRenderer.prototype.pause = function(pause) {
 sre.SsmlRenderer.prototype.prosodyElement = function(attr, value) {
   value = this.applyScaleFunction(value);
   var valueStr = value < 0 ? value.toString() : '+' + value;
-  return '<prosody ' + attr.toLowerCase() + '="' + valueStr +
+  return '<prosody ' + attr.toLowerCase() + '="' + Math.floor(valueStr) +
       (attr === sre.Engine.personalityProps.VOLUME ? '>' : '%">');
 };
 
