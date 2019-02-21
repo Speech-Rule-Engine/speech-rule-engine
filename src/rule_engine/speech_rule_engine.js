54d564d84d17aeb03f79eb0c93cc873fa009dca2
zorkow <volker.sorge@gmail.com>
2019-02-21 19:19:57 +0000

    Removes review comment.

--- src/rule_engine/speech_rule_engine.js
+++ src/rule_engine/speech_rule_engine.js
@@ -426,8 +426,7 @@ sre.SpeechRuleEngine.prototype.addPersonality_ = function(
     this.addRelativePersonality_(descr, personality);
     this.addExternalAttributes_(descr, node);
   }
-  // MOSS: Removes the last joiner in a multi node element. This should be
-  //       reviewed.
+  // Removes the last joiner in a multi node element.
   if (multi && descrs.length) {
     delete descrs[descrs.length - 1].
         personality[sre.Engine.personalityProps.JOIN];
