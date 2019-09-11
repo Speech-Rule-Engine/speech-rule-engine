0fc18c22b8f63dc7d3188e933ff9bce23227211c
zorkow <volker.sorge@gmail.com>
2019-09-11 20:02:54 +0100

    Removes all resources except www.

--- resources/samples/mathspeak/html/transform-script.sh
+++
@@ -1,9 +0,0 @@
-for i in chap*.tex; do
-perl -i.bak -pe 's/&\n//' $i;
-perl -i.bak -pe 's/bottomrule/hline/' $i;
-perl -i.bak -pe 's/end{longtable}/end{tabular}/' $i;
-perl -i.bak -pe 's/toprule\\addlinespace/\hline/' $i;
-perl -i.bak -pe 's/\\addlinespace/[1ex]/' $i;
-perl -i.bak -pe 's/\\begin{longtable}\[c\]{@\{}lll@\{}}/\n\\begin{tabular}{lp{12cm}}/' $i;
-perl -i.bak -pe 's/\\begin{longtable}\[c\]{@\{}ll@\{}}/\n\\begin{tabular}{lp{12cm}}/' $i;
-done
