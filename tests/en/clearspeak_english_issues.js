ecad93e2773f3f806b819816289894eaad749684
zorkow <volker.sorge@gmail.com>
2020-02-08 22:15:36 +0000

    Adds tests for issue 320.

--- tests/en/clearspeak_english_issues.js
+++ tests/en/clearspeak_english_issues.js
@@ -67,3 +67,41 @@ sre.ClearspeakEnglishIssues.prototype.testIssueNumbers = function() {
       '<mrow><mn>#FFFF</mn></mrow>',
       'number number sign F F F F', preference);
 };
+
+
+
+
+/**
+ * Tests for issue #320 and related expressions.
+ */
+sre.ClearspeakEnglishIssues.prototype.testIssue320 = function() {
+  this.executeRuleTest(
+      '<mo>∂</mo>', 'partial-differential');
+  this.executeRuleTest(
+      '<mi>x</mi><mo>!</mo>', 'x factorial');
+  this.executeRuleTest(
+      '<mo>-</mo><mo>-</mo><mi>x</mi>', 'negative negative x');
+  this.executeRuleTest(
+      '<mo>+</mo><mo>+</mo><mi>x</mi>', 'plus plus x');
+  this.executeRuleTest(
+      '<mi>sin</mi><mi>sin</mi><mi>x</mi>', 'sine sine x');
+  this.executeRuleTest(
+      '<mo>∂</mo><mo>∂</mo><mi>x</mi>',
+      'partial-differential partial-differential x');
+  this.executeRuleTest(
+      '<mrow><mrow><mo>(</mo><mfrac><msup><mo>∂</mo><mn>2</mn></msup>' +
+      '<mrow><mo>∂</mo><msup><mi>x</mi><mn>2</mn></msup></mrow></mfrac>' +
+      '<mo>+</mo><mfrac><msup><mo>∂</mo><mn>2</mn></msup><mrow><mo>∂</mo>' +
+      '<msup><mi>y</mi><mn>2</mn></msup></mrow></mfrac><mo>)</mo></mrow>' +
+      '<msup><mrow><mo minsize="150%">|</mo><mi>φ <!-- \varphi --></mi>' +
+      '<mo stretchy="false">(</mo><mi>x</mi><mo>+</mo>' +
+      '<mi mathvariant="normal">i</mi><mi>y</mi><mo stretchy="false">)</mo>' +
+      '<mo minsize="150%">|</mo></mrow><mn>2</mn></msup><mo>=</mo>' +
+      '<mn>0</mn></mrow>',
+      'open paren, the fraction with numerator partial-differential squared, ' +
+      'and denominator, the partial-differential of x squared, plus, the ' +
+      'fraction with numerator  partial-differential squared, and ' +
+      'denominator, the partial-differential of y squared, close paren, ' +
+      'times, the absolute value of phi times, open paren, x plus normal i, ' +
+      'y, close paren, squared equals 0');
+};
