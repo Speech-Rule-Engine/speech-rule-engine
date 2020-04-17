b7c727c4b97b1382a4b6e9229f05348986b039ff
zorkow <volker.sorge@gmail.com>
2020-04-17 13:01:42 +0100

    Updates old mathml rule tests.

--- tests/mathml_rule_test.js
+++ tests/mathml_rule_test.js
@@ -51,11 +51,11 @@ goog.inherits(sre.MathmlRuleTest, sre.AbstractRuleTest);
 sre.MathmlRuleTest.prototype.testMathmlEmpty = function() {
   this.executeRuleTest('<mspace depth="40px"/>', '');
   this.executeRuleTest(
-      '<mstyle displaystyle="true"><mn>1</mn></mstyle>', 'digit one');
+      '<mstyle displaystyle="true"><mn>1</mn></mstyle>', 'one');
   this.executeRuleTest(
-      '<mpadded height="+150px"><mn>1</mn></mpadded>', 'digit one');
-  this.executeRuleTest('<merror><mn>1</mn></merror>', 'digit one');
-  this.executeRuleTest('<mphantom><mn>1</mn></mphantom>', 'digit one');
+      '<mpadded height="+150px"><mn>1</mn></mpadded>', 'one');
+  this.executeRuleTest('<merror><mn>1</mn></merror>', 'one');
+  this.executeRuleTest('<mphantom><mn>1</mn></mphantom>', 'one');
 };
 
 
@@ -64,11 +64,11 @@ sre.MathmlRuleTest.prototype.testMathmlEmpty = function() {
  * @export
  */
 sre.MathmlRuleTest.prototype.testMathmlToken = function() {
-  this.executeRuleTest('<mi>x</mi>', 'latin small letter x');
+  this.executeRuleTest('<mi>x</mi>', 'x');
   this.executeRuleTest('<mi mathvariant="normal" class="MathML-Unit">km</mi>',
-                       'kilometers');
-  this.executeRuleTest('<mn>1</mn>', 'digit one');
-  this.executeRuleTest('<mo>+</mo>', 'plus sign');
+                       'kilometer');
+  this.executeRuleTest('<mn>1</mn>', 'one');
+  this.executeRuleTest('<mo>+</mo>', 'plus');
   this.executeRuleTest('<mtext>therefore we have</mtext>', 'therefore we have');
   this.executeRuleTest('<ms>therefore we have</ms>',
                        'string therefore we have');
@@ -82,19 +82,19 @@ sre.MathmlRuleTest.prototype.testMathmlToken = function() {
 sre.MathmlRuleTest.prototype.testMathmlFont = function() {
 
   this.executeRuleTest('<mi mathvariant="normal">N</mi>',
-                       'latin capital letter n');
+                       'cap N');
   this.executeRuleTest('<mi mathvariant="double-struck">N</mi>',
-                       'double-struck latin capital letter n');
+                       'double-struck cap N');
 
   this.executeRuleTest('<mn mathvariant="normal">1</mn>',
-                       'digit one');
+                       'one');
   this.executeRuleTest('<mn mathvariant="fraktur">1</mn>',
-                       'fraktur digit one');
+                       'fraktur one');
 
   this.executeRuleTest('<mo mathvariant="normal">+</mo>',
-                       'plus sign');
+                       'plus');
   this.executeRuleTest('<mo mathvariant="monospace">+</mo>',
-                       'monospace plus sign');
+                       'monospace plus');
 
   this.executeRuleTest(
       '<mtext mathvariant="normal">therefore we have</mtext>',
@@ -112,26 +112,23 @@ sre.MathmlRuleTest.prototype.testMathmlFont = function() {
 sre.MathmlRuleTest.prototype.testMathmlScript = function() {
 
   this.executeRuleTest('<msup><mi>R</mi><mi>n</mi></msup>',
-                       'latin capital letter r super latin small letter n');
+                       'cap R super n');
   this.executeRuleTest('<msub><mi>Z</mi><mi>n</mi></msub>',
-                       'latin capital letter z sub latin small letter n');
+                       'cap Z sub n');
   this.executeRuleTest(
       '<msubsup><mi>Z</mi><mi>n</mi><mi>r</mi></msubsup>',
-      'latin capital letter z sub latin small letter n super latin small' +
-      ' letter r');
+      'cap Z sub n super r');
   this.executeRuleTest(
       '<munder><mrow><mi>x</mi><mo>+</mo><mi>y</mi></mrow>' +
       '<mo>&#x23DF;</mo></munder>',
-      'bottom curly bracket under latin small letter x plus sign latin small' +
-      ' letter y');
+      'bottom brace under x plus y');
   this.executeRuleTest(
       '<mover><mrow><mi>x</mi><mo>+</mo><mi>y</mi></mrow>' +
       '<mo>&#x23DE;</mo></mover>',
-      'top curly bracket over latin small letter x plus sign latin small' +
-      ' letter y');
+      'top brace over x plus y');
   this.executeRuleTest(
       '<munderover><mo>&#x222B;</mo><mn>0</mn><mi>&#x221E;</mi></munderover>',
-      'digit zero under and infinity over integral');
+      'zero under and infinity over integral');
 };
 
 
@@ -141,13 +138,13 @@ sre.MathmlRuleTest.prototype.testMathmlScript = function() {
  */
 sre.MathmlRuleTest.prototype.testMathmlLayout = function() {
   this.executeRuleTest('<mrow><mi>x</mi><mo>+</mo><mi>y</mi></mrow>',
-                       'latin small letter x plus sign latin small letter y');
+                       'x plus y');
   this.executeRuleTest('<msqrt><mi>x</mi></msqrt>',
-                       'Square root of latin small letter x');
+                       'Square root of x');
   this.executeRuleTest('<mroot><mi>x</mi><mn>3</mn></mroot>',
-                       'root of order digit three of latin small letter x');
+                       'root of order three of x');
   this.executeRuleTest('<mfrac><mn>1</mn><mi>n</mi></mfrac>',
-                       'digit one divided by latin small letter n');
+                       'one divided by n');
 };
 
 
@@ -158,30 +155,30 @@ sre.MathmlRuleTest.prototype.testMathmlLayout = function() {
 sre.MathmlRuleTest.prototype.testMathmlSpecialization = function() {
 
   this.executeRuleTest('<msup><mi>Z</mi><mn>2</mn></msup>',
-                       'latin capital letter z square');
+                       'cap Z square');
   this.executeRuleTest(
       '<msup><mi>Z</mi><mrow><mn>2</mn></mrow></msup>',
-      'latin capital letter z square');
+      'cap Z square');
 
   this.executeRuleTest('<msup><mi>Z</mi><mn>3</mn></msup>',
-                       'latin capital letter z cube');
+                       'cap Z cube');
   this.executeRuleTest(
       '<msup><mi>Z</mi><mrow><mn>3</mn></mrow></msup>',
-      'latin capital letter z cube');
+      'cap Z cube');
 
   this.executeRuleTest(
       '<msubsup><mi>Z</mi><mi>n</mi><mn>2</mn></msubsup>',
-      'latin capital letter z sub latin small letter n square');
+      'cap Z sub n square');
   this.executeRuleTest(
       '<msubsup><mi>Z</mi><mi>n</mi><mrow><mn>2</mn></mrow></msubsup>',
-      'latin capital letter z sub latin small letter n square');
+      'cap Z sub n square');
 
   this.executeRuleTest(
       '<msubsup><mi>Z</mi><mi>n</mi><mn>3</mn></msubsup>',
-      'latin capital letter z sub latin small letter n cube');
+      'cap Z sub n cube');
   this.executeRuleTest(
       '<msubsup><mi>Z</mi><mi>n</mi><mrow><mn>3</mn></mrow></msubsup>',
-      'latin capital letter z sub latin small letter n cube');
+      'cap Z sub n cube');
 
 };
 
@@ -194,27 +191,23 @@ sre.MathmlRuleTest.prototype.testMathmlMfencedSingleSep = function() {
   this.executeRuleTest(
       '<mfenced open="{" close="}" separators=";"><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left curly bracket latin small letter a semicolon latin small letter b' +
-      ' semicolon latin small letter c semicolon latin small letter d' +
-      ' semicolon latin small letter e right curly bracket');
+      'left brace a semicolon b semicolon c semicolon d semicolon e right' +
+      ' brace');
   this.executeRuleTest(
       '<mfenced close="}" separators=";"><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left parenthesis latin small letter a semicolon latin small letter b' +
-      ' semicolon latin small letter c semicolon latin small letter d' +
-      ' semicolon latin small letter e right curly bracket');
+      'left parenthesis a semicolon b semicolon c semicolon d semicolon e' +
+      ' right brace');
   this.executeRuleTest(
       '<mfenced separators=";"><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left parenthesis latin small letter a semicolon latin small letter b' +
-      ' semicolon latin small letter c semicolon latin small letter d' +
-      ' semicolon latin small letter e right parenthesis');
+      'left parenthesis a semicolon b semicolon c semicolon d semicolon e' +
+      ' right parenthesis');
   this.executeRuleTest(
       '<mfenced open="{" separators=";"><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left curly bracket latin small letter a semicolon latin small letter b' +
-      ' semicolon latin small letter c semicolon latin small letter d' +
-      ' semicolon latin small letter e right parenthesis');
+      'left brace a semicolon b semicolon c semicolon d semicolon e right' +
+      ' parenthesis');
 };
 
 
@@ -226,26 +219,19 @@ sre.MathmlRuleTest.prototype.testMathmlMfencedSpaceSep = function() {
   this.executeRuleTest(
       '<mfenced open="{" close="}" separators=" "><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left curly bracket latin small letter a latin small letter b latin' +
-      ' small letter c latin small letter d latin small letter e right' +
-      ' curly bracket');
+      'left brace a b c d e right brace');
   this.executeRuleTest(
       '<mfenced close="}" separators=" "><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left parenthesis latin small letter a latin small letter b latin small' +
-      ' letter c latin small letter d latin small letter e right curly' +
-      ' bracket');
+      'left parenthesis a b c d e right brace');
   this.executeRuleTest(
       '<mfenced open="{" separators=" "><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left curly bracket latin small letter a latin small letter b latin' +
-      ' small letter c latin small letter d latin small letter e right' +
-      ' parenthesis');
+      'left brace a b c d e right parenthesis');
   this.executeRuleTest(
       '<mfenced separators=" "><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left parenthesis latin small letter a latin small letter b latin small' +
-      ' letter c latin small letter d latin small letter e right parenthesis');
+      'left parenthesis a b c d e right parenthesis');
 };
 
 
@@ -257,26 +243,19 @@ sre.MathmlRuleTest.prototype.testMathmlMfencedEmptySep = function() {
   this.executeRuleTest(
       '<mfenced open="{" close="}" separators=""><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left curly bracket latin small letter a latin small letter b latin' +
-      ' small letter c latin small letter d latin small letter e right' +
-      ' curly bracket');
+      'left brace a b c d e right brace');
   this.executeRuleTest(
       '<mfenced close="}" separators=""><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left parenthesis latin small letter a latin small letter b latin small' +
-      ' letter c latin small letter d latin small letter e right curly' +
-      ' bracket');
+      'left parenthesis a b c d e right brace');
   this.executeRuleTest(
       '<mfenced open="{" separators=""><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left curly bracket latin small letter a latin small letter b latin' +
-      ' small letter c latin small letter d latin small letter e right' +
-      ' parenthesis');
+      'left brace a b c d e right parenthesis');
   this.executeRuleTest(
       '<mfenced separators=""><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left parenthesis latin small letter a latin small letter b latin small' +
-      ' letter c latin small letter d latin small letter e right parenthesis');
+      'left parenthesis a b c d e right parenthesis');
 };
 
 
@@ -288,27 +267,19 @@ sre.MathmlRuleTest.prototype.testMathmlMfencedNoSep = function() {
   this.executeRuleTest(
       '<mfenced open="{" close="}"><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left curly bracket latin small letter a comma latin small letter b' +
-      ' comma latin small letter c comma latin small letter d comma latin' +
-      ' small letter e right curly bracket');
+      'left brace a comma b comma c comma d comma e right brace');
   this.executeRuleTest(
       '<mfenced close="}"><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left parenthesis latin small letter a comma latin small letter b comma' +
-      ' latin small letter c comma latin small letter d comma latin small' +
-      ' letter e right curly bracket');
+      'left parenthesis a comma b comma c comma d comma e right brace');
   this.executeRuleTest(
       '<mfenced open="{"><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left curly bracket latin small letter a comma latin small letter b' +
-      ' comma latin small letter c comma latin small letter d comma latin' +
-      ' small letter e right parenthesis');
+      'left brace a comma b comma c comma d comma e right parenthesis');
   this.executeRuleTest(
       '<mfenced><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left parenthesis latin small letter a comma latin small letter b comma' +
-      ' latin small letter c comma latin small letter d comma latin small' +
-      ' letter e right parenthesis');
+      'left parenthesis a comma b comma c comma d comma e right parenthesis');
 };
 
 
@@ -320,27 +291,20 @@ sre.MathmlRuleTest.prototype.testMathmlMfencedMultipleSep = function() {
   this.executeRuleTest(
       '<mfenced open="{" close="}" separators=";;,"><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left curly bracket latin small letter a semicolon latin small letter b' +
-      ' semicolon latin small letter c comma latin small letter d comma' +
-      ' latin small letter e right curly bracket');
+      'left brace a semicolon b semicolon c comma d comma e right brace');
   this.executeRuleTest(
       '<mfenced close="}" separators=";;,"><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left parenthesis latin small letter a semicolon latin small letter b' +
-      ' semicolon latin small letter c comma latin small letter d comma' +
-      ' latin small letter e right curly bracket');
+      'left parenthesis a semicolon b semicolon c comma d comma e right brace');
   this.executeRuleTest(
       '<mfenced open="{" separators=";;,"><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left curly bracket latin small letter a semicolon latin small letter b' +
-      ' semicolon latin small letter c comma latin small letter d comma' +
-      ' latin small letter e right parenthesis');
+      'left brace a semicolon b semicolon c comma d comma e right parenthesis');
   this.executeRuleTest(
       '<mfenced separators=";;,"><mi>a</mi><mi>b</mi>' +
       '<mi>c</mi><mi>d</mi><mi>e</mi></mfenced>',
-      'left parenthesis latin small letter a semicolon latin small letter b' +
-      ' semicolon latin small letter c comma latin small letter d comma' +
-      ' latin small letter e right parenthesis');
+      'left parenthesis a semicolon b semicolon c comma d comma e right' +
+      ' parenthesis');
 };
 
 
@@ -353,14 +317,13 @@ sre.MathmlRuleTest.prototype.testMathmlMtable = function() {
   this.executeRuleTest(
       '<mtable><mtr><mtd><mi>A</mi></mtd><mtd><mi>B</mi></mtd></mtr>' +
       '<mtr><mtd><mi>C</mi></mtd><mtd><mi>D</mi></mtd></mtr></mtable>',
-      'matrix row 1column 1 latin capital letter a column 2 latin capital' +
-      ' letter b row 2column 1 latin capital letter c column 2 latin' +
-      ' capital letter d');
+      'matrix row 1column 1 cap A column 2 cap B row 2column 1 cap C column' +
+      ' 2 cap D');
   this.executeRuleTest(
       '<mtr><mtd><mi>A</mi></mtd><mtd><mi>B</mi></mtd></mtr>',
-      'column 1 latin capital letter a column 2 latin capital letter b');
+      'column 1 cap A column 2 cap B');
   this.executeRuleTest('<mtd><mi>A</mi></mtd>',
-                       'latin capital letter a');
+                       'cap A');
 };
 
 
@@ -373,41 +336,36 @@ sre.MathmlRuleTest.prototype.testMathmlMultiscript = function() {
   this.executeRuleTest(
       '<mmultiscripts><mi>A</mi><mn>1</mn><mn>2</mn>' +
           '<mprescripts/><mn>3</mn><mn>4</mn></mmultiscripts>',
-      'latin capital letter a left sub digit three left super digit four' +
-      ' right sub digit one right super digit two');
+      'cap A left sub three left super four right sub one right super two');
 
   this.executeRuleTest(
       '<mmultiscripts><mi>A</mi><none/><mn>2</mn>' +
           '<mprescripts/><mn>3</mn><mn>4</mn></mmultiscripts>',
-      'latin capital letter a left sub digit three left super digit four' +
-      ' right super digit two');
+      'cap A left sub three left super four right super two');
   this.executeRuleTest(
       '<mmultiscripts><mi>A</mi><mn>1</mn><none/>' +
           '<mprescripts/><mn>3</mn><mn>4</mn></mmultiscripts>',
-      'latin capital letter a left sub digit three left super digit four' +
-      ' right sub digit one');
+      'cap A left sub three left super four right sub one');
   this.executeRuleTest(
       '<mmultiscripts><mi>A</mi><mn>1</mn><mn>2</mn>' +
           '<mprescripts/><none/><mn>4</mn></mmultiscripts>',
-      'latin capital letter a left super digit four right sub digit one' +
-      ' right super digit two');
+      'cap A left super four right sub one right super two');
   this.executeRuleTest(
       '<mmultiscripts><mi>A</mi><mn>1</mn><mn>2</mn>' +
           '<mprescripts/><mn>3</mn><none/></mmultiscripts>',
-      'latin capital letter a left sub digit three right sub digit one' +
-      ' right super digit two');
+      'cap A left sub three right sub one right super two');
 
   this.executeRuleTest(
       '<mmultiscripts><mi>A</mi><none/><none/>' +
           '<mprescripts/><mn>3</mn><mn>4</mn></mmultiscripts>',
-      'latin capital letter a left sub digit three left super digit four');
+      'cap A left sub three left super four');
 
   this.executeRuleTest(
       '<mmultiscripts><mi>A</mi><none/><none/>' +
           '<mprescripts/><none/><mn>4</mn></mmultiscripts>',
-      'latin capital letter a left super digit four');
+      'cap A left super four');
   this.executeRuleTest(
       '<mmultiscripts><mi>A</mi><none/><none/>' +
           '<mprescripts/><mn>3</mn><none/></mmultiscripts>',
-      'latin capital letter a left sub digit three');
+      'cap A left sub three');
 };
