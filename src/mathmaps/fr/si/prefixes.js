c26d9bfa3bacc9ec9e6d70e6c72503430c6644e7
zorkow <volker.sorge@gmail.com>
2020-10-19 00:41:46 +0100

    Adds missing prefix files.

---
+++ src/mathmaps/fr/si/prefixes.js
@@ -0,0 +1,25 @@
+[
+  {
+    "Y": "yotta",
+    "Z": "zetta",
+    "E": "exa",
+    "P": "péta",
+    "T": "téra",
+    "G": "giga",
+    "M": "méga",
+    "k": "kilo",
+    "h": "hecto",
+    "d": "déca",
+    "d": "déci",
+    "c": "centi",
+    "m": "milli",
+    "µ": "micro",
+    "μ": "micro",
+    "n": "nano",
+    "p": "pico",
+    "f": "femto",
+    "a": "atto",
+    "z": "zepto",
+    "y": "yocto"
+  }
+]
