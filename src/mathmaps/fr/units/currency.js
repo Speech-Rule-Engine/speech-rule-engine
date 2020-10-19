715632c344203ed5501f2ff230bc2294102cad5b
zorkow <volker.sorge@gmail.com>
2020-10-19 02:33:25 +0100

    Adds French currencies.

---
+++ src/mathmaps/fr/units/currency.js
@@ -0,0 +1,378 @@
+[
+  {
+    "locale": "fr"
+  },
+  {
+    "locale": "fr"
+  },
+  {
+    "key": "$",
+    "mappings": {
+      "default": {
+        "default": "dollars",
+        "singular": "dollar",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "$",
+      "💲",
+      "＄",
+      "﹩",
+      "USD"
+    ]
+  },
+  {
+    "key": "£",
+    "mappings": {
+      "default": {
+        "default": "pee",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "£",
+      "￡",
+      "GBP"
+    ]
+  },
+  {
+    "key": "¥",
+    "mappings": {
+      "default": {
+        "default": "yen",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "¥",
+      "￥",
+      "JPY"
+    ]
+  },
+  {
+    "key": "€",
+    "mappings": {
+      "default": {
+        "default": "euros",
+        "singular": "euro",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "€",
+      "EUR"
+    ]
+  },
+  {
+    "key": "₡",
+    "mappings": {
+      "default": {
+        "default": "colon",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₡",
+      "CRC"
+    ]
+  },
+  {
+    "key": "₢",
+    "mappings": {
+      "default": {
+        "default": "cruzeiro",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₢"
+    ]
+  },
+  {
+    "key": "₣",
+    "mappings": {
+      "default": {
+        "default": "francs",
+        "singular": "franc",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₣"
+    ]
+  },
+  {
+    "key": "₤",
+    "mappings": {
+      "default": {
+        "default": "lires",
+        "singular": "lire",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₤"
+    ]
+  },
+  {
+    "key": "₥",
+    "mappings": {
+      "default": {
+        "default": "millième",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₥"
+    ]
+  },
+  {
+    "key": "₦",
+    "mappings": {
+      "default": {
+        "default": "naira",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₦",
+      "NGN"
+    ]
+  },
+  {
+    "key": "₧",
+    "mappings": {
+      "default": {
+        "default": "peseta",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₧"
+    ]
+  },
+  {
+    "key": "₨",
+    "mappings": {
+      "default": {
+        "default": "roupies",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₨",
+      "₹",
+      "INR",
+      "NPR",
+      "PKR",
+      "LKR"
+    ]
+  },
+  {
+    "key": "₩",
+    "mappings": {
+      "default": {
+        "default": "won",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₩",
+      "￦",
+      "KRW"
+    ]
+  },
+  {
+    "key": "₪",
+    "mappings": {
+      "default": {
+        "default": "chékel",
+        "singular": "chékel",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₪"
+    ]
+  },
+  {
+    "key": "₫",
+    "mappings": {
+      "default": {
+        "default": "dong",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₫"
+    ]
+  },
+  {
+    "key": "₭",
+    "mappings": {
+      "default": {
+        "default": "kip",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₭"
+    ]
+  },
+  {
+    "key": "₮",
+    "mappings": {
+      "default": {
+        "default": "tugrik",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₮"
+    ]
+  },
+  {
+    "key": "₯",
+    "mappings": {
+      "default": {
+        "default": "drachme",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₯"
+    ]
+  },
+  {
+    "key": "₰",
+    "mappings": {
+      "default": {
+        "default": "pfennig",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₰"
+    ]
+  },
+  {
+    "key": "₱",
+    "mappings": {
+      "default": {
+        "default": "pesos",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₱"
+    ]
+  },
+  {
+    "key": "₲",
+    "mappings": {
+      "default": {
+        "default": "guaranis",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₲"
+    ]
+  },
+  {
+    "key": "₳",
+    "mappings": {
+      "default": {
+        "default": "australs",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₳"
+    ]
+  },
+  {
+    "key": "₴",
+    "mappings": {
+      "default": {
+        "default": "hryvnias",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₴",
+      "UAH"
+    ]
+  },
+  {
+    "key": "₵",
+    "mappings": {
+      "default": {
+        "default": "cedis",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "₵",
+      "GHS"
+    ]
+  },
+  {
+    "key": "元",
+    "mappings": {
+      "default": {
+        "default": "yuan",
+        "singular": "",
+        "dual": ""
+      }
+    },
+    "category": "currency",
+    "names": [
+      "元"
+    ]
+  }
+]
