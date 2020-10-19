d0f8691ef9489dbd0d5fa03ebc8a3cce5e587c95
zorkow <volker.sorge@gmail.com>
2020-10-19 03:37:03 +0100

    Adds area units to all locales.

---
+++ src/mathmaps/fr/units/area.js
@@ -0,0 +1,76 @@
+[
+  {"locale": "fr"},
+  {"category": "area",
+   "mappings": {
+     "default": {
+       "default": "carrée"
+     }
+   },
+   "key": "sq",
+   "names": ["sq", "sq."]
+  },
+  {"category": "area",
+   "mappings": {
+     "default": {
+       "default": "pouce carré"
+     }
+   },
+   "key": "sq inch",
+   "names": ["sq inch", "sq. inch."]
+  },
+  {"category": "area",
+   "mappings": {
+     "default": {
+       "default": "rod carré"
+     }
+   },
+   "key": "sq rd",
+   "names": ["sq rd", "sq. rd."]
+  },
+  {"category": "area",
+   "mappings": {
+     "default": {
+       "default": "pied carrés",
+       "plural": "pieds carrés"
+     }
+   },
+   "key": "sq ft",
+   "names": ["sq ft", "sq. ft."]
+  },
+  {"category": "area",
+   "mappings": {
+     "default": {
+       "default": "square yard"
+     }
+   },
+   "key": "sq yd",
+   "names": ["sq yd", "sq. yd."]
+  },
+  {"category": "area",
+   "mappings": {
+     "default": {
+       "default": "mile carré"
+     }
+   },
+   "key": "sq mi",
+   "names": ["sq mi", "sq. mi."]
+  },
+  {"category": "area",
+   "mappings": {
+     "default": {
+       "default": "acre"
+     }
+   },
+   "key": "acr",
+   "names": ["ac", "ac.", "acr", "acr."]
+  },
+  {"category": "area",
+   "mappings": {
+     "default": {
+       "default": "hectare"
+     }
+   },
+   "key": "ha",
+   "names": ["ha"]
+  }
+]
