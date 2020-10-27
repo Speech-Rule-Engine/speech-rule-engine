c3a8f84402cf41282292ae97a2de17127e0b7c71
zorkow <volker.sorge@gmail.com>
2020-10-27 18:23:58 +0000

    Improves ms tensor constraints.

--- src/speech_rules/mathspeak_util.js
+++ src/speech_rules/mathspeak_util.js
@@ -825,7 +825,6 @@ sre.MathspeakUtil.generateTensorRules = function(store) {
   //
   //  lsub lsuper base rsub rsuper
   var defineRule = goog.bind(store.defineRule, store);
-  var defineRulesAlias = goog.bind(store.defineRulesAlias, store);
   var defineSpecialisedRule = goog.bind(store.defineSpecialisedRule, store);
   var constellations = ['11111', '11110', '11101', '11100',
                         '10111', '10110', '10101', '10100',
@@ -849,23 +848,16 @@ sre.MathspeakUtil.generateTensorRules = function(store) {
     verbStr += '; [t]' + baselineStr + 'Verbose';
     briefStr += '; [t]' + baselineStr + 'Brief';
     name = name + '-baseline';
-    verbList = [name, 'default', verbStr, 'self::tensor',
-                'following-sibling::*'].
+    var cstr = '(.//*[not(*)])[last()]!=((.//ancestor::fraction|' +
+        'ancestor::root|ancestor::sqrt|ancestor::cell|ancestor::line|' +
+        'ancestor::stree)[1]//*[not(*)])[last()]';
+    verbList = [name, 'default', verbStr, 'self::tensor', cstr].
         concat(components);
-    briefList = [name, 'brief', briefStr, 'self::tensor',
-                 'following-sibling::*'].
+    briefList = [name, 'brief', briefStr, 'self::tensor', cstr].
         concat(components);
     defineRule.apply(null, verbList);
     defineRule.apply(null, briefList);
     defineSpecialisedRule(name, 'brief', 'sbrief');
-    // Rules without neighbour but baseline.
-    var aliasList = [name, 'self::tensor', 'not(following-sibling::*)',
-                     'ancestor::fraction|ancestor::punctuated|' +
-                     'ancestor::fenced|ancestor::root|ancestor::sqrt|' +
-                     'ancestor::relseq|ancestor::multirel|' +
-                     '@embellished'].
-        concat(components);
-    defineRulesAlias.apply(null, aliasList);
   }
 };
 
