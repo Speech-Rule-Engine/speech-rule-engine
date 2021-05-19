67ae6f6172a3c62d9534d9bda770e406e528d220
zorkow <volker.sorge@gmail.com>
2021-05-19 19:09:34 +0100

    Version bump.

--- ts/common/variables.ts
+++ ts/common/variables.ts
@@ -25,7 +25,7 @@ export class Variables {
   /**
    * SRE version.
    */
-  public static readonly VERSION: string = '4.0.0-alpha.0';
+  public static readonly VERSION: string = '4.0.0-alpha.1';
 
 
   /**
