1c90170f0ceb589d9f40fc0d3c01c354f44fd48b
DCheol <70943743+DCheol@users.noreply.github.com>
2021-06-28 02:29:16 +0900

    Add files via upload

---
+++ src/mathmaps/ko/symbols/math_harpoons.js
@@ -0,0 +1,470 @@
+[{"locale": "en"},
+  {
+    "category": "So",
+    "mappings": {
+      "default": {
+        "default": "바브 위 왼쪽으로 작살"//"left harpoon with barb up"
+      }
+    },
+    "key": "21BC"
+  },
+  {
+    "category": "So",
+    "mappings": {
+      "default": {
+        "default": "바브 아래 왼쪽으로 작살"//"left harpoon with barb down"
+      }
+    },
+    "key": "21BD"
+  },
+  {
+    "category": "So",
+    "mappings": {
+      "default": {
+        "default": "바브 오른 위쪽으로 작살"//"up harpoon with barb right"
+      }
+    },
+    "key": "21BE"
+  },
+  {
+    "category": "So",
+    "mappings": {
+      "default": {
+        "default": "바브 왼 위쪽으로 작살"//"up harpoon with barb left"
+      }
+    },
+    "key": "21BF"
+  },
+  {
+    "category": "So",
+    "mappings": {
+      "default": {
+        "default": "바브 위 오른쪽으로 작살"//"right harpoon with barb up"
+      }
+    },
+    "key": "21C0"
+  },
+  {
+    "category": "So",
+    "mappings": {
+      "default": {
+        "default": "바브 아래 오른쪽으로 작살"//"right harpoon with barb down"
+      }
+    },
+    "key": "21C1"
+  },
+  {
+    "category": "So",
+    "mappings": {
+      "default": {
+        "default": "바브 오른 아래쪽으로 작살"//"down harpoon with barb right"
+      }
+    },
+    "key": "21C2"
+  },
+  {
+    "category": "So",
+    "mappings": {
+      "default": {
+        "default": "바브 왼 아래쪽으로 작살"//"down harpoon with barb left"
+      }
+    },
+    "key": "21C3"
+  },
+  {
+    "category": "So",
+    "mappings": {
+      "default": {
+        "default": "오른쪽 작살 위 왼쪽 작살"//"left harpoon over right harpoon"
+      }
+    },
+    "key": "21CB"
+  },
+  {
+    "category": "So",
+    "mappings": {
+      "default": {
+        "default": "왼쪽 작살 위 오른쪽 작살"//"right harpoon over left harpoon"
+      }
+    },
+    "key": "21CC"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "왼쪽 바브 오른쪽 위로 바브 다운 하푼"//"left barb up right barb down harpoon"
+      }
+    },
+    "key": "294A"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "왼쪽 미늘 왼쪽 미늘 미늘 위로"//"left barb down right barb up harpoon"
+      }
+    },
+    "key": "294B"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "바브 바로 아래로 바브 왼쪽으로 작살"//"up barb right down barb left harpoon"
+      }
+    },
+    "key": "294C"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "Up Barb 왼쪽 아래 Barb Right Harpoon"//"up barb left down barb right harpoon"
+      }
+    },
+    "key": "294D"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "왼쪽 바브 오른쪽 위로 바브 위로 하푼"//"left barb up right barb up harpoon"
+      }
+    },
+    "key": "294E"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "바브 바로 아래로 바브 라이트 하푼"//"up barb right down barb right harpoon"
+      }
+    },
+    "key": "294F"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "왼쪽 미늘봉 아래로 오른쪽 미늘봉 아래로"//"left barb down right barb down harpoon"
+      }
+    },
+    "key": "2950"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "바브 왼쪽 아래로 바브 왼쪽 하푼"//"up barb left down barb left harpoon"
+      }
+    },
+    "key": "2951"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "왼쪽으로 작살과 막대기까지 바브"//"left harpoon with barb up to bar"
+      }
+    },
+    "key": "2952"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "버프 업 바를 가진 오른쪽 하푼"//"right harpoon with barb up to bar"
+      }
+    },
+    "key": "2953"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "바브 바로 가기 하푼"//"up harpoon with barb right to bar"
+      }
+    },
+    "key": "2954"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "down harpoon with barb right to bar"
+      }
+    },
+    "key": "2955"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "left harpoon with barb down to bar"
+      }
+    },
+    "key": "2956"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "right harpoon with barb down to bar"
+      }
+    },
+    "key": "2957"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "up harpoon with barb left to bar"
+      }
+    },
+    "key": "2958"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "down harpoon with barb left to bar"
+      }
+    },
+    "key": "2959"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "left harpoon with barb up from bar"
+      }
+    },
+    "key": "295A"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "right harpoon with barb up from bar"
+      }
+    },
+    "key": "295B"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "up harpoon with barb right from bar"
+      }
+    },
+    "key": "295C"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "down harpoon with barb right from bar"
+      }
+    },
+    "key": "295D"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "left harpoon with barb down from bar"
+      }
+    },
+    "key": "295E"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "right harpoon with barb down from bar"
+      }
+    },
+    "key": "295F"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "up harpoon with barb left from bar"
+      }
+    },
+    "key": "2960"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "down harpoon with barb left from bar"
+      }
+    },
+    "key": "2961"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "left harpoon with barb up above left harpoon with barb down"
+      }
+    },
+    "key": "2962"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "up harpoon with barb left beside up harpoon with barb right"
+      }
+    },
+    "key": "2963"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "right harpoon with barb up above right harpoon with barb down"
+      }
+    },
+    "key": "2964"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "down harpoon with barb left beside down harpoon with barb right"
+      }
+    },
+    "key": "2965"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "left harpoon with barb up above right harpoon with barb up"
+      }
+    },
+    "key": "2966"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "left harpoon with barb down above right harpoon with barb down"
+      }
+    },
+    "key": "2967"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "right harpoon with barb up above left harpoon with barb up"
+      }
+    },
+    "key": "2968"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "right harpoon with barb down above left harpoon with barb down"
+      }
+    },
+    "key": "2969"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "left harpoon with barb up above long dash"
+      }
+    },
+    "key": "296A"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "left harpoon with barb down below long dash"
+      }
+    },
+    "key": "296B"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "right harpoon with barb up above long dash"
+      }
+    },
+    "key": "296C"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "right harpoon with barb down below long dash"
+      }
+    },
+    "key": "296D"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "up harpoon with barb left beside down harpoon with barb right"
+      }
+    },
+    "key": "296E"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "down harpoon with barb left beside up harpoon with barb right"
+      }
+    },
+    "key": "296F"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "left fish tail"
+      }
+    },
+    "key": "297C"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "right fish tail"
+      }
+    },
+    "key": "297D"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "up fish tail"
+      }
+    },
+    "key": "297E"
+  },
+  {
+    "category": "Sm",
+    "mappings": {
+      "default": {
+        "default": "down fish tail"
+      }
+    },
+    "key": "297F"
+  }
+]
