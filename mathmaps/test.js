72ceee56dd096802cab21959ca3381ce200e2522
DCheol <70943743+DCheol@users.noreply.github.com>
2021-07-13 17:43:24 +0900

    Delete test.js

--- mathmaps/test.js
+++
@@ -1,3 +0,0 @@
-var sre = require('speech-rule-engine');
-
-console.log(sre.toSpeech('<math xmlns="http://www.w3.org/1998/Math/MathML"><math><mrow><mn>1</mn><mo>,</mo><mtext></mtext><mn>2</mn><mo>,</mo><mtext></mtext><mn>3</mn><mo>,</mo><mtext></mtext><mo>…</mo><mtext></mtext><mo>,</mo><mn>20</mn></mrow></math></math>'));
