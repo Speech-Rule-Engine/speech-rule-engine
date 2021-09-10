bd4d94933545120dc6662ddb7b9c8f95d9869b35
zorkow <volker.sorge@gmail.com>
2021-09-10 03:51:16 +0200

    No standard number algorithm.

--- ts/l10n/locales/locale_nb.ts
+++ ts/l10n/locales/locale_nb.ts
@@ -24,7 +24,7 @@
 //
 
 import {createLocale, Locale} from '../locale';
-import NUMBERS from '../numbers/numbers_nb';
+import NUMBERS from '../numbers/numbers_nn';
 import * as tr from '../transformers';
 
 let locale: Locale = null;
--- ts/l10n/numbers/numbers_nn.ts
+++ ts/l10n/numbers/numbers_nn.ts
@@ -1,5 +1,5 @@
 //
-// Copyright 2020-21 Volker Sorge
+// Copyright 2019-21 Volker Sorge
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,31 +14,12 @@
 // limitations under the License.
 
 /**
- * @fileoverview Translating numbers into Bokmal.
+ * @fileoverview Translating numbers into Nynorsk.
  * @author volker.sorge@gmail.com (Volker Sorge)
  */
+
+
 import {Numbers, NUMBERS as NUMB} from '../messages';
-
-//
-// This work was sponsored by TextHelp
-//
-
-/**
- * Sub-ISO specification. Possible values: fr, be, sw.
- */
-export let SUB_ISO: string = 'alt';
-
-/**
- * Changes number one 'eins' into a prefix.
- * @param num number string.
- * @return If it is a one, it is made into prefix.
- */
-function onePrefix_(num: string, thd: boolean = false): string {
-  let numOne = NUMBERS.ones[1];
-  return num === numOne ?
-    (num === 'ein' ? 'eitt ' : (thd ? 'et' : 'ett'))
-    : num;
-}
 
 
 /**
@@ -49,27 +30,71 @@ function onePrefix_(num: string, thd: boolean = false): string {
 function hundredsToWords_(num: number, ordinal: boolean = false): string {
   let n = num % 1000;
   let str = '';
-  let ones = NUMBERS.ones[Math.floor(n / 100)];
-  str += ones ? onePrefix_(ones) + 'hundre' : '';
+  let count = Math.floor(n / 100);
+  let ones = NUMBERS.ones[count];
+  str += ones ? (count === 1 ? '' : ones) + 'hundre' : '';
   n = n % 100;
   if (n) {
     str += str ? 'og' : '';
     if (ordinal) {
       let ord = NUMBERS.special.smallOrdinals[n];
       if (ord) {
-        return str += ord;
+        return str + ord;
+      }
+      if (n % 10) {
+        return str + NUMBERS.tens[Math.floor(n / 10)] +
+          NUMBERS.special.smallOrdinals[n % 10];
       }
     }
-    ones = NUMBERS.ones[n];
-    if (ones) {
-      str += ones;
-    } else {
-      let tens = NUMBERS.tens[Math.floor(n / 10)];
-      ones = NUMBERS.ones[n % 10];
-      str += ones ? ones + 'og' + tens : tens;
-    }
+    str += NUMBERS.ones[n] ||
+        NUMBERS.tens[Math.floor(n / 10)] +
+            (n % 10 ? NUMBERS.ones[n % 10] : '');
   }
   return (ordinal ? replaceOrdinal(str) : str);
+}
+
+
+/**
+ * Translates a number of up to twelve digits into a string representation.
+ * @param num The number to translate.
+ * @return The string representation of that number.
+ */
+function numberToWords(num: number, ordinal: boolean = false): string {
+  if (num === 0) {
+    return NUMBERS.zero;
+  }
+  if (num >= Math.pow(10, 36)) {
+    return num.toString();
+  }
+  let pos = 0;
+  let str = '';
+  while (num > 0) {
+    let hundreds = num % 1000;
+    if (hundreds) {
+      let hund = hundredsToWords_(num % 1000, (pos ? false : ordinal));
+      if (!pos && ordinal) {
+        ordinal = !ordinal;
+      }
+      str = hund + (pos ? ' ' +
+        (NUMBERS.large[pos] + (pos > 1 && hundreds > 1 ? 'er' : ''))
+          + ' ' : '') + str;
+    }
+    num = Math.floor(num / 1000);
+    pos++;
+  }
+  return ordinal ? (str + (str.match(/tusen$/) ? 'de' : 'te')) : str;
+}
+
+
+/**
+ * Translates a number of up to twelve digits into a string representation of
+ * its ordinal.
+ * @param num The number to translate.
+ * @param plural A flag indicating if the ordinal is in plural.
+ * @return The ordinal of the number as string.
+ */
+function numberToOrdinal(num: number, _plural: boolean): string {
+  return wordOrdinal(num);
 }
 
 
@@ -93,57 +118,9 @@ function replaceOrdinal(str: string): string {
     return str.slice(0, -1) + NUMBERS.special.endOrdinal;
   }
   if (str.match(/e$/)) {
-    return str + 'nde';
-  }
+    return str + 'nde'; 
+ }
   return str + 'nde';
-}
-
-
-/**
- * Translates a number of up to twelve digits into a string representation.
- * @param num The number to translate.
- * @return The string representation of that number.
- */
-function numberToWords(num: number, ordinal: boolean = false): string {
-  if (num === 0) {
-    return ordinal ? NUMBERS.special.smallOrdinals[0] : NUMBERS.zero;
-  }
-  if (num >= Math.pow(10, 36)) {
-    return num.toString();
-  }
-  let pos = 0;
-  let str = '';
-  while (num > 0) {
-    let hundreds = num % 1000;
-    if (hundreds) {
-      let hund = hundredsToWords_(num % 1000, (pos ? false : ordinal));
-      if (!pos && ordinal) {
-        ordinal = !ordinal;
-      }
-      str = (pos === 1 ? onePrefix_(hund, true) : hund) +
-        (pos > 1 ? NUMBERS.numSep : '') +
-        (pos ?
-          // If this is million or above take care oaf the plural.
-          (NUMBERS.large[pos] + (pos > 1 && hundreds > 1 ? 'er' : '')) :
-          '') +
-        (pos > 1 && str ? NUMBERS.numSep : '') + str;
-    }
-    num = Math.floor(num / 1000);
-    pos++;
-  }
-  return ordinal ? (str + (str.match(/tusen$/) ? 'de' : 'te')) : str;
-}
-
-
-/**
- * Translates a number of up to twelve digits into a string representation of
- * its ordinal.
- * @param num The number to translate.
- * @param plural A flag indicating if the ordinal is in plural.
- * @return The ordinal of the number as string.
- */
-function numberToOrdinal(num: number, _plural: boolean): string {
-  return wordOrdinal(num);
 }
 
 
@@ -167,7 +144,6 @@ function simpleOrdinal(num: number): string {
   return num.toString() + '.';
 }
 
-
 const NUMBERS: Numbers = NUMB();
 NUMBERS.wordOrdinal = wordOrdinal;
 NUMBERS.simpleOrdinal = simpleOrdinal;
@@ -175,3 +151,4 @@ NUMBERS.numberToWords = numberToWords;
 NUMBERS.numberToOrdinal = numberToOrdinal;
 
 export default NUMBERS;
+// TODO: For simple speech output this should be different.
