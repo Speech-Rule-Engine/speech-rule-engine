0acdb68f0033e9c97c873d268c91d7f3e59ea859
aintbe <drwho@g.skku.edu>
2021-10-31 14:23:48 +0900

    ContentIteratorArticle Test5

--- ts/l10n/locales/locale_ko.ts
+++ ts/l10n/locales/locale_ko.ts
@@ -54,13 +54,13 @@ function create(): Locale {
     if (last.match(/[r,l,n,m,1,3,6,7,8,0]/i)) result = true;
     Grammar.getInstance().setParameter('last', result);
 
-    return name+result;
+    return name;//+result;
   }
   loc.CORRECTIONS.article = (name: string) => {
     let last = Grammar.getInstance().getParameter('last');
     if (last) name = {'는': '은', '와': '과', '를': '을', '로': '으로'}[name];
     
-    return name;
+    return name+last;
   }
   return loc;
 }
