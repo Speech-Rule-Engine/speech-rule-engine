e992a8becf72440cf03dae10bbace79a3cf1021b
hantaihe <80453189+hantaihe@users.noreply.github.com>
2021-11-26 13:41:58 +0900

    Modify Codes

--- ts/l10n/locales/locale_ko.ts
+++ ts/l10n/locales/locale_ko.ts
@@ -55,17 +55,14 @@ function create(): Locale {
   loc.CORRECTIONS.postposition = (name: string) => {
     let final = name.slice(-1);
     let char = (final.charCodeAt(0) - 44032) % 28;
-
     let result = (char > 0) ? true : false;
     if (final.match(/[r,l,n,m,1,3,6,7,8,0]/i)) result = true;
     Grammar.getInstance().setParameter('final', result);
-
     return name;
   }
     loc.CORRECTIONS.article = (name: string) => {
     let final = Grammar.getInstance().getParameter('last');
     let temp = name.split(" ");
-
     if (final) temp[0] = {'는': '은', '와': '과', '를': '을', '로': '으로'}[temp[0]];
     return (temp[0] !== undefined)? temp.join(" ") : name;
   }
