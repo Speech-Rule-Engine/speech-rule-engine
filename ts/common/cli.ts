7c554cff893390b1cd8cf64a4892052a9cc6a565
zorkow <volker.sorge@gmail.com>
2021-12-16 23:52:52 +0100

    Refactors global file namespace.

--- ts/common/cli.ts
+++ ts/common/cli.ts
@@ -161,7 +161,7 @@ export class Cli {
     const options = SystemExternal.commander.opts();
     EnginePromise.getall().then(() => {
       this.runProcessors_((proc, file) => {
-        console.info(System.file.processFile(proc, file, options.output));
+        console.info(System.processFile(proc, file, options.output));
       }, input);
     });
   }
--- ts/common/system.ts
+++ ts/common/system.ts
@@ -59,9 +59,9 @@ export async function setupEngine(feature: {
   // we get a meaningful output.
   if (
     feature.domain === 'default' &&
-    (feature.modality === 'speech' ||
-      !feature.modality ||
-      engine.modality === 'speech')
+      (feature.modality === 'speech' ||
+        !feature.modality ||
+        engine.modality === 'speech')
   ) {
     feature.domain = 'mathspeak';
   }
@@ -96,7 +96,7 @@ export async function setupEngine(feature: {
 function config(feature: { [key: string]: boolean | string }) {
   if (
     Engine.getInstance().mode === EngineConst.Mode.HTTP &&
-    !Engine.getInstance().config
+      !Engine.getInstance().config
   ) {
     configBlocks(feature);
     Engine.getInstance().config = true;
@@ -237,118 +237,121 @@ function processString<T>(processor: string, input: string): T {
   return ProcessorFactory.process(processor, input);
 }
 
-export namespace file {
-  /**
-   * Reads an xml expression from a file and returns its aural rendering to a
-   * file.
-   *
-   * @param input The input filename.
-   * @param opt_output The output filename if one is given.
-   * @return Promise that resolves on completion of the file operations.
-   */
-  export function toSpeech(input: string, opt_output?: string) {
-    return processFile('speech', input, opt_output);
-  }
+/**
+ * Namespace for file processing methods.
+ */
+export const file: Record<string, (input: string, output?: string) => any> = {};
 
-  /**
-   * Reads an xml expression from a file and returns the XML for the semantic
-   * tree to a file.
-   *
-   * @param input The input filename.
-   * @param opt_output The output filename if one is given.
-   * @return Promise that resolves on completion of the file operations.
-   */
-  export function toSemantic(input: string, opt_output?: string) {
-    return processFile('semantic', input, opt_output);
-  }
+/**
+ * Reads an xml expression from a file and returns its aural rendering to a
+ * file.
+ *
+ * @param input The input filename.
+ * @param opt_output The output filename if one is given.
+ * @return Promise that resolves on completion of the file operations.
+ */
+file.toSpeech = function(input: string, opt_output?: string) {
+  return processFile('speech', input, opt_output);
+}
 
-  /**
-   * Function to translate MathML string into JSON version of the Semantic Tree
-   * to a file.
-   *
-   * @param input The input filename.
-   * @param opt_output The output filename if one is given.
-   * @return Promise that resolves on completion of the file operations.
-   */
-  export function toJson(input: string, opt_output?: string) {
-    return processFile('json', input, opt_output);
-  }
+/**
+ * Reads an xml expression from a file and returns the XML for the semantic
+ * tree to a file.
+ *
+ * @param input The input filename.
+ * @param opt_output The output filename if one is given.
+ * @return Promise that resolves on completion of the file operations.
+ */
+file.toSemantic= function(input: string, opt_output?: string) {
+  return processFile('semantic', input, opt_output);
+}
 
-  /**
-   * Main function to translate expressions into auditory descriptions
-   * a file.
-   *
-   * @param input The input filename.
-   * @param opt_output The output filename if one is given.
-   * @return Promise that resolves on completion of the file operations.
-   */
-  export function toDescription(input: string, opt_output?: string) {
-    return processFile('description', input, opt_output);
-  }
+/**
+ * Function to translate MathML string into JSON version of the Semantic Tree
+ * to a file.
+ *
+ * @param input The input filename.
+ * @param opt_output The output filename if one is given.
+ * @return Promise that resolves on completion of the file operations.
+ */
+file.toJson= function(input: string, opt_output?: string) {
+  return processFile('json', input, opt_output);
+}
 
-  /**
-   * Function to translate MathML string into semantically enriched MathML in a
-   * file.
-   *
-   * @param input The input filename.
-   * @param opt_output The output filename if one is given.
-   * @return Promise that resolves on completion of the file operations.
-   */
-  export function toEnriched(input: string, opt_output?: string) {
-    return processFile('enriched', input, opt_output);
-  }
+/**
+ * Main function to translate expressions into auditory descriptions
+ * a file.
+ *
+ * @param input The input filename.
+ * @param opt_output The output filename if one is given.
+ * @return Promise that resolves on completion of the file operations.
+ */
+file.toDescription= function(input: string, opt_output?: string) {
+  return processFile('description', input, opt_output);
+}
 
-  /**
-   * Reads an xml expression from a file, processes with the given function and
-   * returns the result either to a file or to stdout.
-   *
-   * @param processor The name of the processor to call.
-   * @param input The input filename.
-   * @param opt_output The output filename if one is given.
-   * @return The promise for the file process to complete.
-   */
-  export function processFile(
-    processor: string,
-    input: string,
-    opt_output?: string
-  ) {
-    switch (Engine.getInstance().mode) {
-      case EngineConst.Mode.ASYNC:
-        return processFileAsync(processor, input, opt_output);
-      case EngineConst.Mode.SYNC:
-        return processFileSync(processor, input, opt_output);
-      default:
-        throw new SREError(
-          `Can process files in ${Engine.getInstance().mode} mode`
-        );
+/**
+ * Function to translate MathML string into semantically enriched MathML in a
+ * file.
+ *
+ * @param input The input filename.
+ * @param opt_output The output filename if one is given.
+ * @return Promise that resolves on completion of the file operations.
+ */
+file.toEnriched= function(input: string, opt_output?: string) {
+  return processFile('enriched', input, opt_output);
+}
+
+/**
+ * Reads an xml expression from a file, processes with the given function and
+ * returns the result either to a file or to stdout.
+ *
+ * @param processor The name of the processor to call.
+ * @param input The input filename.
+ * @param opt_output The output filename if one is given.
+ * @return The promise for the file process to complete.
+ */
+export function processFile(
+  processor: string,
+  input: string,
+  opt_output?: string
+) {
+  switch (Engine.getInstance().mode) {
+    case EngineConst.Mode.ASYNC:
+      return processFileAsync(processor, input, opt_output);
+    case EngineConst.Mode.SYNC:
+      return processFileSync(processor, input, opt_output);
+    default:
+      throw new SREError(
+        `Can process files in ${Engine.getInstance().mode} mode`
+      );
+  }
+}
+
+/**
+ * Synchronously reads an xml expression from a file, processes with the given
+ * function and returns the result either to a file or to stdout in synchronous
+ * mode.
+ *
+ * @param processor The name of the processor.
+ * @param input The input filename.
+ * @param opt_output The output filename if one is given.
+ */
+export function processFileSync(
+  processor: string,
+  input: string,
+  opt_output?: string
+) {
+  const expr = inputFileSync_(input);
+  const result = ProcessorFactory.output(processor, expr);
+  if (opt_output) {
+    try {
+      SystemExternal.fs.writeFileSync(opt_output, result);
+    } catch (err) {
+      throw new SREError('Can not write to file: ' + opt_output);
     }
   }
-
-  /**
-   * Synchronously reads an xml expression from a file, processes with the given
-   * function and returns the result either to a file or to stdout in synchronous
-   * mode.
-   *
-   * @param processor The name of the processor.
-   * @param input The input filename.
-   * @param opt_output The output filename if one is given.
-   */
-  export function processFileSync(
-    processor: string,
-    input: string,
-    opt_output?: string
-  ) {
-    const expr = inputFileSync_(input);
-    const result = ProcessorFactory.output(processor, expr);
-    if (opt_output) {
-      try {
-        SystemExternal.fs.writeFileSync(opt_output, result);
-      } catch (err) {
-        throw new SREError('Can not write to file: ' + opt_output);
-      }
-    }
-    return result;
-  }
+  return result;
 }
 
 /**
@@ -455,5 +458,5 @@ if (SystemExternal.documentSupported) {
 } else {
   setupEngine({ mode: EngineConst.Mode.SYNC }).then(() =>
     setupEngine({ mode: EngineConst.Mode.ASYNC })
-  );
+                                                   );
 }
