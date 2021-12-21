20136a2ac8c1bf3c0d2830ad2a8b07bfdcd0bf49
zorkow <volker.sorge@gmail.com>
2021-12-21 17:39:57 +0100

    Beta version bump.

--- ts/common/variables.ts
+++ ts/common/variables.ts
@@ -22,7 +22,7 @@ export class Variables {
   /**
    * SRE version.
    */
-  public static readonly VERSION: string = '4.0.0-beta.3';
+  public static readonly VERSION: string = '4.0.0-beta.4';
 
   /**
    * List of locales to load.
