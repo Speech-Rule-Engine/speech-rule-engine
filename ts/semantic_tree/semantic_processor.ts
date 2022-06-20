7b802256ce8cf8609619f8ee666ca5dfeea5a056
zorkow <volker.sorge@gmail.com>
2022-06-21 00:02:00 +0200

    Improves reproducibility of unit heuristic.

--- ts/semantic_tree/semantic_processor.ts
+++ ts/semantic_tree/semantic_processor.ts
@@ -2225,6 +2225,10 @@ export default class SemanticProcessor {
       prefix
     );
     if (!split.div) {
+      // Check for unit here?
+      if (SemanticPred.isUnitProduct(node)) {
+        node.role = SemanticRole.UNIT;
+      }
       return node;
     }
     return SemanticProcessor.getInstance().operationsTree_(
