6ca88a1224b2db4ebf9b04765e6df1d6468ecadb
zorkow <volker.sorge@gmail.com>
2024-04-03 21:55:11 +0200

    setup and speech generation in xplat module.

--- ts/xplatmath.ts
+++ ts/xplatmath.ts
@@ -2,10 +2,22 @@ import { xmlTree, xmlTreeFromString, getTree, getTreeFromString } from './semant
 import { SpeechRuleEngine, EngineInstance } from './rule_engine/speech_rule_engine.js';
 import { setup } from './common/engine_setup.js';
 import { enginePromise } from './common/engine.js';
+import { markup, finalize } from './audio/aural_rendering.js';
 
 declare let Sre: any;
 
-Sre = {};
+Sre = {
+  'startup': function(init: {[key: string]: string}) {
+    setup({'mode': 'sync'});
+    setup(init);
+  },
+  'tree': function(input: string) {
+    return xmlTreeFromString(input);
+  },
+  'speech': function(input: string) {
+    return finalize(markup(EngineInstance().evaluateNode(xmlTreeFromString(input))));
+  }
+};
 Sre.xmlTree = xmlTree;
 Sre.xmlTreeFromString = xmlTreeFromString;
 Sre.getTree = getTree;
