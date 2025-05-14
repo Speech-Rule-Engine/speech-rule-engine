00669babb7b4fc4b0c872e0f8b20c2a8ff10b1cb
zorkow <volker.sorge@gmail.com>
2025-05-14 17:30:35 +0200

    alpha version bump

--- ts/common/variables.ts
+++ ts/common/variables.ts
@@ -22,7 +22,7 @@ export class Variables {
   /**
    * SRE version.
    */
-  public static readonly VERSION: string = '5.0.0-alpha.1';
+  public static readonly VERSION: string = '5.0.0-alpha.2';
 
   /**
    * Locale mapping to language names.
@@ -69,7 +69,7 @@ export class Variables {
    * MathJax version. This indicates the lowest MathJax version this version of
    * SRE is compatible with.
    */
-  public static readonly mathjaxVersion: string = '4.0.0-beta.5';
+  public static readonly mathjaxVersion: string = '4.0.0-beta.8';
 
   /**
    * The URL for SRE resources.
