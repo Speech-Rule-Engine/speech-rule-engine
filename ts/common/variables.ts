104abf99519a18fda23a984368abc7daa511168a
zorkow <volker.sorge@gmail.com>
2025-06-25 17:29:55 +0200

    alpha version bump.

--- ts/common/variables.ts
+++ ts/common/variables.ts
@@ -22,7 +22,7 @@ export class Variables {
   /**
    * SRE version.
    */
-  public static readonly VERSION: string = '5.0.0-alpha.7';
+  public static readonly VERSION: string = '5.0.0-alpha.8';
 
   /**
    * Locale mapping to language names.
