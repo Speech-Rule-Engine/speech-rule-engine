b2cf48c976fee1833195cf36af3d896e86d397a8
zorkow <volker.sorge@gmail.com>
2025-08-04 18:06:53 +0200

    reinstate mathspeak for english

--- ts/speech_rules/clearspeak_preference_string.ts
+++ ts/speech_rules/clearspeak_preference_string.ts
@@ -58,7 +58,7 @@ export const PREFERENCES = new DynamicProperties({
     // Reciprocal is French
     'Reciprocal'
   ],
-  Inference: ['Auto', 'Short'],
+  Inference: ['Auto', 'Long'],
   ImpliedTimes: ['Auto', 'MoreImpliedTimes', 'None'],
   Log: ['Auto', 'LnAsNaturalLog'],
   Matrix: [
