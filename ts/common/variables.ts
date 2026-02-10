29d3229b1292770449b824c831c64e1cda265b3c
zorkow <volker.sorge@gmail.com>
2026-02-10 15:22:30 +0100

    beta version bump

--- ts/common/variables.ts
+++ ts/common/variables.ts
@@ -22,7 +22,7 @@ export class Variables {
   /**
    * SRE version.
    */
-  public static readonly VERSION: string = '5.0.0-beta.3';
+  public static readonly VERSION: string = '5.0.0-beta.4';
 
   /**
    * Locale mapping to language names.
