1555916feccca8798df21ee0628c793a4559b259
zorkow <volker.sorge@gmail.com>
2026-04-21 19:21:54 +0200

    patch version bump

--- ts/common/variables.ts
+++ ts/common/variables.ts
@@ -22,7 +22,7 @@ export class Variables {
   /**
    * SRE version.
    */
-  public static readonly VERSION: string = '4.1.3';
+  public static readonly VERSION: string = '4.1.4';
 
   /**
    * Locale mapping to language names.
