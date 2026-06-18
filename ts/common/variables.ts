6cdb84ae2e2c9373114ce6ce228780d8d49a84b3
zorkow <volker.sorge@gmail.com>
2026-06-18 13:39:46 +0200

    rc version bump

--- ts/common/variables.ts
+++ ts/common/variables.ts
@@ -22,7 +22,7 @@ export class Variables {
   /**
    * SRE version.
    */
-  public static readonly VERSION: string = '5.0.0-rc.1';
+  public static readonly VERSION: string = '5.0.0-rc.2';
 
   /**
    * Locale mapping to language names.
